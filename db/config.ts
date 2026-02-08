82892195a5dc4e48c8e51166cb766da998a11760
Xetha666 <bryans27092000@gmail.com>
2026-02-08 09:37:59 -0500

    feat: define User table with columns in database configuration

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,16 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable, NOW } from 'astro:db';
 
-// https://astro.build/db/config
+const User = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    username: column.text({ unique: true }),
+    password: column.text(),
+    name: column.text(),
+    role: column.text(),
+    createdAt: column.date({default : NOW}),
+  }
+})
+
 export default defineDb({
-  tables: {}
+  tables: {User}
 });
