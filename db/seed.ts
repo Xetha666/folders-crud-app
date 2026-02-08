a1af9851a65dc18b07e5ee3ad790015d1f9752f2
Xetha666 <bryans27092000@gmail.com>
2026-02-08 09:38:10 -0500

    feat: implement seed function to insert default users with hashed passwords

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,38 @@
-import { db } from 'astro:db';
+import { db, User } from 'astro:db';
+import bcrypt from 'bcryptjs';
+import 'dotenv/config';
 
-// https://astro.build/db/seed
+
 export default async function seed() {
-	// TODO
+  const saltRounds = 10;
+  const DEFAULT_PASSWORD = process.env.DEFAULT_PASSWORD;
+  const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || DEFAULT_PASSWORD;
+
+  if (!DEFAULT_PASSWORD) {
+    throw new Error("ERROR: No se encontró DEFAULT_PASSWORD en el archivo .env");
+  }
+  if (!ADMIN_PASSWORD) {
+    throw new Error("ERROR: No se encontró ADMIN_PASSWORD en el archivo .env");
+  }
+
+  const adminHashedPassword = await bcrypt.hash(ADMIN_PASSWORD, saltRounds);
+  const hashedPassword = await bcrypt.hash(DEFAULT_PASSWORD, saltRounds);
+
+  await db.insert(User).values([
+    {
+      id: crypto.randomUUID(), 
+      username: "useradmin",
+      password: adminHashedPassword,
+      name:"User Admin",
+      role: "admin",
+    },
+    {
+      id: crypto.randomUUID(),
+      username:"test",
+      password:hashedPassword,
+      name: "Test User",
+      role: "user",
+    }
+  ]);
 }
+
