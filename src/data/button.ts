4932be9a9eeec760c514a9aef5d6c54446de7db5
Xetha666 <bryans27092000@gmail.com>
2026-02-12 23:24:25 -0500

    feat: add input and example fields to Button actions for enhanced user guidance

--- src/data/button.ts
+++ src/data/button.ts
@@ -3,6 +3,10 @@ export interface Button {
   label: string;
   icon: string;
   colorClass: string;
+  firstInput: string;
+  firstExample: string;
+  secondInput: string;
+  secondExample: string;
 }
 
 export const ACTIONS: Button[] = [
@@ -11,17 +15,29 @@ export const ACTIONS: Button[] = [
     label: "Procedencia",
     icon: "IconProcedencia",
     colorClass: "btn-secondary border-secondary shadow-secondary/20 hover:shadow-secondary/40",
+    firstInput:"Ingrese el nombre corto de la procedencia",
+    firstExample:"Ejemplo: 1ª FS Chanchamayo",
+    secondInput:"Ingrese el nombre completo de la procedencia",
+    secondExample:"Ejemplo: Primera Fiscalía Superior de Chanchamayo",
   },
   {
     id: "modal_instructor",
     label: "Instructores",
     icon: "IconInstructor",
     colorClass: "btn-info border-info shadow-info/20 hover:shadow-info/40",
+    firstInput:"Ingrese el nombre del instructor",
+    firstExample:"Ejemplo: SANTILLAN ROJAS Edgard Paul",
+    secondInput:"Ingrese el grado del instructor",
+    secondExample:"Ejemplo: TNTE, SS, SB",
   },
   {
     id: "modal_fiscal",
     label: "Fiscales",
     icon: "IconInstructor",
     colorClass: "btn-error border-error shadow-error/20 hover:shadow-error/40",
+    firstInput:"Ingrese el nombre del fiscal",
+    firstExample:"Ejemplo: Janeth ALMEYDA ESCOBAR",
+    secondInput:"Ingrese el genero del fiscal",
+    secondExample:"Ejemplo: Femenino o Masculino",
   }
 ];
